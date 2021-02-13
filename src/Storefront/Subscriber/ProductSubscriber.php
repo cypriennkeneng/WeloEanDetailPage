0300e3f7c5a5fca537951e8987d1c431360ae958
webloupe <info@webloupe.de>
2021-02-13 12:51:19 +0100

    initial commit

--- src/Storefront/Subscriber/ProductSubscriber.php
+++ src/Storefront/Subscriber/ProductSubscriber.php
@@ -134,14 +134,4 @@ class ProductSubscriber implements EventSubscriberInterface
             ]
         );
     }
-
-    /**
-     * Helper method for debugging
-     * @param $data
-     */
-    function dumb($data){
-        highlight_string("<?php\n " . var_export($data, true) . "?>");
-        echo '<script>document.getElementsByTagName("code")[0].getElementsByTagName("span")[1].remove() ;document.getElementsByTagName("code")[0].getElementsByTagName("span")[document.getElementsByTagName("code")[0].getElementsByTagName("span").length - 1].remove() ; </script>';
-        die();
-    }
 }
